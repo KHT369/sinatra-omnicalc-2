c3636dc7bd7d039e889f80d10871f35100ec6912
KHT369 <ktsao@chicagobooth.edu>
2024-10-26 00:28:11 +0000

    save 1

--- app.rb
+++ app.rb
@@ -1,18 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_numb = params.fetch("first_num").to_f
+  @second_numb = params.fetch("second_num").to_f
 
-  @result = @first_num + @second_num
+  @result = @first_numb + @second_numb
   erb(:add_result)
 end
 
@@ -21,17 +28,23 @@ get("/subtract") do
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+  @first_numb = params.fetch("first_num").to_f
+  @second_numb = params.fetch("second_num").to_f
 
+  @result = @second_numb - @first_numb 
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_numb = params.fetch("first_num").to_f
+  @second_numb = params.fetch("second_num").to_f
+
+  @result = @first_numb * @second_numb
+
   erb(:mult_result)
 end
 
@@ -40,5 +53,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_numb = params.fetch("first_num").to_f
+  @second_numb = params.fetch("second_num").to_f
+
+  @result = @first_numb / @second_numb
+
   erb(:div_result)
 end
